d70f49ae25a1791b200cf3a7ec6ae1d3934340a0
Frank <FrankssWu@outlook.com>
2022-10-03 22:57:56 +0800

    add LoginPage

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,11 @@ const router = createRouter({
             component: RepositoryView
         },
         {
+            path: "/login",
+            name: "login",
+            component: () => import("@/views/LoginView.vue")
+        },
+        {
             path: "/issue",
             name: "issue",
             component: () => import("@/views/IssueListView.vue")
---
+++ src/views/LoginView.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="login-page">
+        <div class="login-box">
+            <div class="avatar-box">
+                <img src="../assets/logo.svg" alt="">
+            </div>
+            <h2 class="dynamic-rainbow">Sign in to XYN Hub</h2>
+            <el-card class="main-card" :body-style="{ padding: '20px' }" shadow="hover">
+                <el-form label-width="0px" class="login-in" ref="loginForm" :model="loginForm" :rules="rules">
+                    <p>Username or email address</p>
+                    <el-form-item prop="userName">
+                        <el-input type="text" v-model="loginForm.userName"></el-input>
+                    </el-form-item>
+                    <p>Password <a class="forget-pass" href="/forget">Forgot password?</a></p>
+                    <el-form-item prop="passWord">
+                        <el-input type="password" v-model="loginForm.passWord"></el-input>
+                    </el-form-item>
+                    <el-form-item class="button">
+                        <el-button class="button-signin" type="success">Sign in</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-card>
+            <el-card class="new-card" :body-style="{ padding: '20px' }" shadow="hover">
+                New to XYN Hub? <a href="/regisiter">Create an account.</a>
+            </el-card>
+        </div>
+    </div>
+</template>
+  
+<script lang="ts">
+export default {
+    name: "Login",
+    data() {
+        return {
+            loginForm: {
+                userName: '',
+                passWord: ''
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.login-page {
+    margin: 0 auto;
+}
+
+.login-page .login-box .button {
+    margin-top: -10px;
+}
+
+.login-page .login-box .button-signin {
+    width: 100%;
+}
+
+.login-page .login-box .forget-pass {
+    float: right;
+}
+
+.login-page .main-card {
+    /*background-image: -webkit-linear-gradient(left, #00aaff, #ff84e0, #ffffff, #ffffff);
+    -webkit-background-size: 200% 100%;
+    -webkit-animation: maskedAnimation 8s infinite linear;*/
+    background-color: aliceblue;
+}
+
+.login-page .login-box .new-card {
+    margin-top: 10px;
+    text-align: center;
+    font-size: var(--el-font-size-small);
+
+}
+
+.login-page .login-box .dynamic-rainbow {
+    margin-top: 10px;
+    background-image: -webkit-linear-gradient(left, #00aaff, #ff84e0, #ffffff, #ffffff);
+    -webkit-text-fill-color: transparent;
+    -webkit-background-size: 200% 100%;
+    -webkit-background-clip: text;
+    -webkit-animation: maskedAnimation 8s infinite linear;
+    font-size: 32px;
+}
+</style>
