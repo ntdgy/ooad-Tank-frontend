9b4405bf472db091d5687df3890ac566852cc7fe
Fros1er <34234343+Fros1er@users.noreply.github.com>
2022-10-14 17:19:08 +0800

    hide header buttons on none repo routes

--- src/components/repo/RepoHeader.vue
+++ src/components/repo/RepoHeader.vue
@@ -2,34 +2,37 @@
     <div>
         <el-breadcrumb>
             <el-breadcrumb-item>{{ username }}</el-breadcrumb-item>
-            <el-breadcrumb-item :to="{name: 'repo'}">{{ reponame }}</el-breadcrumb-item>
+            <el-breadcrumb-item :to="getRepoRouteTarget()">{{ reponame }}</el-breadcrumb-item>
             <el-breadcrumb-item v-for="(path, idx) in filteredPath" :key="path" :to="getRouteTarget(idx)">{{path}}
             </el-breadcrumb-item>
         </el-breadcrumb>
         <el-divider />
 
-        <!--buttons-->
-        <Toolbar>
-            <template #left>
-                <el-select v-model="currentBranch" filterable>
-                    <el-option-group label="type to search">
-                        <el-option v-for="branch of computedBranches" :key="branch" :label="branch" :value="branch" />
-                    </el-option-group>
-                </el-select>
-            </template>
-            <template #right>
-                <el-button>Add File&nbsp;<el-icon>
-                        <Plus />
-                    </el-icon>
-                </el-button>
-                <el-button type="primary">Clone&nbsp;<el-icon>
-                        <ArrowDown />
-                    </el-icon>
-                </el-button>
-                <!--TODO: use dropdown here-->
-            </template>
-        </Toolbar>
-        <el-divider />
+        <template v-if="showRepoBar">
+            <!--buttons-->
+            <Toolbar>
+                <template #left>
+                    <el-select v-model="currentBranch" filterable>
+                        <el-option-group label="type to search">
+                            <el-option v-for="branch of computedBranches" :key="branch" :label="branch"
+                                :value="branch" />
+                        </el-option-group>
+                    </el-select>
+                </template>
+                <template #right>
+                    <el-button>Add File&nbsp;<el-icon>
+                            <Plus />
+                        </el-icon>
+                    </el-button>
+                    <el-button type="primary">Clone&nbsp;<el-icon>
+                            <ArrowDown />
+                        </el-icon>
+                    </el-button>
+                    <!--TODO: use dropdown here-->
+                </template>
+            </Toolbar>
+            <el-divider />
+        </template>
     </div>
 </template>
 
@@ -53,6 +56,9 @@ export default defineComponent({
             let res = [...this.$route.params.path]
             if (res[res.length - 1] == '') res.pop()
             return res
+        },
+        showRepoBar() {
+            return ['repo', 'tree', 'blob'].includes(this.$route.name as string)
         }
     },
     watch: {
@@ -67,8 +73,6 @@ export default defineComponent({
                 if (now == this.defaultBranch) return
                 target = 'tree'
             }
-            console.log(now)
-            console.log(target)
             this.$router.push({
                 name: target as string,
                 params: {
@@ -78,18 +82,26 @@ export default defineComponent({
             })
         }
     },
-    created() {
-        this.$watch(() => this.$route.name,
-            (name: string) => {
-                console.log(name)
-            })
-    },
     methods: {
         getRouteTarget(index: number) {
             return index == this.filteredPath.length - 1 ? undefined : {
                 name: 'tree', params: {
                     path: this.filteredPath.slice(0, index + 1)
                 }
+            }
+        },
+        getRepoRouteTarget() {
+            if (this.currentBranch != this.defaultBranch) {
+                return {
+                    name: 'tree',
+                    params: {
+                        branch: this.currentBranch,
+                        path: ['']
+                    }
+                }
+            }
+            return {
+                name: 'repo'
             }
         }
     }
