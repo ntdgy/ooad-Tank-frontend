f8d34af9c7ad32032483c71b331df00626356468
Frank <FrankssWu@outlook.com>
2022-11-15 14:17:30 +0800

    upd setting page, add general-setting, upd collaborator-setting. Still lots todo:(

--- src/views/CollaboratorsSettingView.vue
+++ src/views/CollaboratorsSettingView.vue
@@ -1,39 +1,54 @@
 <template>
-    <div class="general-setting-div">
-        <el-divider content-position="left">
-            <div class="div-title">Who has access</div>
-        </el-divider>
-        <span> 1.提醒状态 2.提醒目前有的collaborators </span>
-        <el-divider content-position="left">
-            <div class="div-title">Manage access</div>
-        </el-divider>
-        <div class="collabortar-head">
-            <el-input :value="modelValue" @input="$emit('update:modelValue', ($event as any).target.value)"
-                placeholder="Find a collaborator" class="input-with-select" clearable @clear="$emit('onClear')">
-                <template #prepend>
-                    <el-select v-model="select" placeholder="Type" style="width: 6rem">
-                        <el-option label="All" value="all" />
-                        <el-option label="Collaborators" value="collaborator" />
-                        <el-option label="Padding Invitations" value="padding" />
-                    </el-select>
-                </template>
-                <template #append>
-                    <el-button>
-                        <el-icon>
-                            <Search />
-                        </el-icon>
-                    </el-button>
-                </template>
-            </el-input>
-            <el-button style="margin-left: 1rem" type="success" @click="addPeople">Add people</el-button>
-        </div>
-        <br />
-        <div class="collabortar-body">
-            <CollaboratorEntry v-for="issue in issues" :id="issue.repo_issue_id" :title="issue.title"
-                :key="issue.repo_issue_id" :issuer="issue.issuer.name" :status="issue.status"
-                :created-at="issue.created_at" :updated-at="issue.updated_at" />
-        </div>
+  <div class="general-setting-div">
+    <el-divider content-position="left"
+      ><div class="div-title">Who has access</div></el-divider
+    >
+    <span> 1.提醒状态 2.提醒目前有的collaborators </span>
+    <el-divider content-position="left"
+      ><div class="div-title">Manage access</div></el-divider
+    >
+    <div class="collabortar-head">
+      <el-input
+        :value="modelValue"
+        @input="$emit('update:modelValue', ($event as any).target.value)"
+        placeholder="Find a collaborator"
+        class="input-with-select"
+        clearable
+        @clear="$emit('onClear')"
+      >
+        <template #prepend>
+          <el-select v-model="select" placeholder="Type" style="width: 6rem">
+            <el-option label="All" value="all" />
+            <el-option label="Collaborators" value="collaborator" />
+            <el-option label="Padding Invitations" value="padding" />
+          </el-select>
+        </template>
+        <template #append>
+          <el-button>
+            <el-icon>
+              <Search />
+            </el-icon>
+          </el-button>
+        </template>
+      </el-input>
+      <el-button style="margin-left: 1rem" type="success" @click="addPeople"
+        >Add people</el-button
+      >
     </div>
+    <br />
+    <div class="collabortar-body">
+      <CollaboratorEntry
+        v-for="issue in issues"
+        :id="issue.repo_issue_id"
+        :title="issue.title"
+        :key="issue.repo_issue_id"
+        :issuer="issue.issuer.name"
+        :status="issue.status"
+        :created-at="issue.created_at"
+        :updated-at="issue.updated_at"
+      />
+    </div>
+  </div>
 </template>
 
 
@@ -68,7 +83,7 @@ export default defineComponent({
         next((vm) => {
             vm.axios
                 .get(
-                    `${baseUrl}/api/repo/${vm.$route.params.username}/${vm.$route.params.reponame}/issue`,
+                    `${baseUrl}/api/repo/${vm.$route.params.username}/${vm.$route.params.reponame}/settings/collaborator`,
                     {
                         withCredentials: true
                     }
@@ -76,7 +91,6 @@ export default defineComponent({
                 .then((res) => res.data.data)
                 .then((data) => {
                     (vm as any).issues = data
-                    console.log(data)
                 })
         })
     }
@@ -85,25 +99,23 @@ export default defineComponent({
 
 <style scoped>
 .toolbar {
-    display: flex;
+  display: flex;
 }
 
 .el-button-group {
-    display: flex;
+  display: flex;
 }
-
 .general-setting-div {
-    margin-top: 2rem;
-    display: flex;
-    flex-direction: column;
-    flex-grow: 1;
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
 }
 
 .general-setting-div .div-title {
-    font-size: 1.5rem;
+  font-size: 1.5rem;
 }
-
 .general-setting-div .collabortar-head {
-    display: flex;
+  display: flex;
 }
 </style>
--- src/views/GeneralSettingView.vue
+++ src/views/GeneralSettingView.vue
@@ -1,28 +1,159 @@
 <template>
   <div class="general-setting-div">
-    <span>What you are you do not see, what you see is your shadow. </span>
-    <el-divider content-position="left">Rabindranath Tagore</el-divider>
-    <span
-      >My wishes are fools, they shout across thy song, my Master. Let me but
-      listen.</span
-    >
-    <el-divider>
-      <el-icon><star-filled /></el-icon>
+    <el-divider content-position="left">
+      <div class="div-title">General</div>
     </el-divider>
-    <span>I cannot choose the best. The best chooses me.</span>
-    <el-divider content-position="right">Rabindranath Tagore</el-divider>
+    <el-form
+      label-nameForm="0px"
+      ref="loginForm"
+      :model="nameForm"
+      :rules="rules"
+    >
+      <p>Repository name</p>
+      <el-form-item prop="name">
+        <el-input type="text" v-model="nameForm.name"></el-input>
+      </el-form-item>
+      <el-button class="button-signin" type="" @click="submit"
+        >Rename</el-button
+      >
+    </el-form>
+    <el-divider content-position="left">
+      <div class="div-title">Danger Zone</div>
+    </el-divider>
+    <div class="repo-box">
+      <el-card
+        class="new-card"
+        :body-style="{ padding: '20px' }"
+        shadow="hover"
+      >
+        <div class="general-setting-text">
+          <h3>Change Visibility</h3>
+          This repository is currently {{ this.$route.params.reponame }} <!--todo: show different choice from repo data-->
+        </div>
+        <el-form-item class="button">
+          <el-button class="button-signin" type="danger" @click="delete_repo"
+            >Change Visibility</el-button
+          >
+        </el-form-item>
+      </el-card>
+      <el-card
+        class="new-card"
+        :body-style="{ padding: '20px' }"
+        shadow="hover"
+      >
+        <div class="general-setting-text">
+          <h3>Delete this repository</h3>
+          Once you delete a repository, there is no going back. Please be
+          certain.
+        </div>
+        <el-form-item class="button">
+          <el-button class="button-signin" type="danger" @click="delete_repo"
+            >Delete This Repository</el-button
+          >
+        </el-form-item>
+      </el-card>
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup>
-import { StarFilled } from "@element-plus/icons-vue"
+<script lang="ts">
+import { defineComponent } from "vue"
+import { ElNotification } from "element-plus"
+import { baseUrl } from "@/stores/configs"
+import type { RepoDesc } from "@/libs/api"
+
+export default defineComponent({
+    data() {
+        return {
+            nameForm: {
+                name: `${this.$route.params.reponame}`
+            },
+            repoForm: {
+                name: `${this.$route.params.reponame}`
+            },
+            repos: Array<RepoDesc>() // todo: fetch repo
+        }
+    },
+    methods: {
+        delete_repo() {},
+        submit() {
+            this.axios
+                .post(
+                    `${baseUrl}/api/repo/${this.$route.params.username}/${this.$route.params.reponame}/updateMetaData`,
+                    this.nameForm,
+                    {
+                        withCredentials: true,
+                        headers: {
+                            "Content-Type": "application/json"
+                        }
+                    }
+                )
+                .then((data) => {
+                    let code = data.data.status.code
+                    if (code == 200 || code == -1002) {
+                        console.log("login success")
+                    } else if (code == -1003) {
+                        ElNotification({
+                            title: "Error",
+                            message: "更新失败",
+                            type: "error"
+                        })
+                    }
+                })
+                .catch((err) => {
+                    ElNotification({
+                        title: "Error",
+                        message: err.code == "ECONNABORTED" ? "更新超时" : "更新失败",
+                        type: "error"
+                    })
+                })
+        }
+    }
+})
 </script>
 
 <style scoped>
+.toolbar {
+  display: flex;
+}
+
+.el-button-group {
+  display: flex;
+}
 .general-setting-div {
   margin-top: 2rem;
   display: flex;
   flex-direction: column;
   flex-grow: 1;
+}
+
+.general-setting-div .div-title {
+  font-size: 1.5rem;
+}
+.general-setting-div .collabortar-head {
+  display: flex;
+}
+
+.general-setting-div .repo-box {
+  width: 50%;
+}
+
+.general-setting-div .repo-box .el-card {
+  display: flex;
+  align-items: center !important;
+  border-color: rgb(231, 138, 138);
+}
+/*
+todo: make button and text in the same line
+text should be on the left
+button must on right
+*/
+.general-setting-div .repo-box .el-button {
+  margin-left: auto;
+  order: 2;
+}
+
+.general-setting-div .repo-box .general-setting-text {
+  width: auto;
 }
 </style>
