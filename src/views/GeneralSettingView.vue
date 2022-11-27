0a6908c3a65b7ac34224c87a3edc2c8e3ef8e430
Frank <FrankssWu@outlook.com>
2022-11-27 12:35:14 +0800

    upd setting page add delete repo (looks terrible)

--- src/views/GeneralSettingView.vue
+++ src/views/GeneralSettingView.vue
@@ -27,8 +27,9 @@
         shadow="hover"
       >
         <div class="general-setting-text">
-          <h3>Change Visibility</h3>
-          This repository is currently {{ this.$route.params.reponame }} <!--todo: show different choice from repo data-->
+          This repository is currently
+          {{ metaData ? (metaData.public ? "public" : "private") : "" }}.
+          <!--todo: show different choice from repo data-->
         </div>
         <el-form-item class="button">
           <el-button class="button-signin" type="danger" @click="delete_repo"
@@ -57,59 +58,103 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
-import { ElNotification } from "element-plus"
-import { baseUrl } from "@/stores/configs"
-import type { RepoDesc } from "@/utils/api"
+import { defineComponent } from "vue";
+import { ElNotification } from "element-plus";
+import { baseUrl } from "@/stores/configs";
 
 export default defineComponent({
-    data() {
-        return {
-            nameForm: {
-                name: `${this.$route.params.reponame}`
-            },
-            repoForm: {
-                name: `${this.$route.params.reponame}`
-            },
-            repos: Array<RepoDesc>() // todo: fetch repo
-        }
+  data() {
+    return {
+      nameForm: {
+        name: `${this.$route.params.reponame}`,
+      },
+      repoForm: {
+        name: `${this.$route.params.reponame}`,
+      },
+      metaData: null,
+    };
+  },
+  methods: {
+    reload() {
+      this.axios
+        .get(
+          `${baseUrl}/api/repo/${this.$route.params.username}/${this.$route.params.reponame}`
+        )
+        .then((res) => res.data.data)
+        .then((data) => {
+          this.metaData = data;
+          console.log(data);
+        });
     },
-    methods: {
-        delete_repo() {},
-        submit() {
-            this.axios
-                .post(
-                    `${baseUrl}/api/repo/${this.$route.params.username}/${this.$route.params.reponame}/updateMetaData`,
-                    this.nameForm,
-                    {
-                        withCredentials: true,
-                        headers: {
-                            "Content-Type": "application/json"
-                        }
-                    }
-                )
-                .then((data) => {
-                    let code = data.data.status.code
-                    if (code == 200 || code == -1002) {
-                        console.log("login success")
-                    } else if (code == -1003) {
-                        ElNotification({
-                            title: "Error",
-                            message: "更新失败",
-                            type: "error"
-                        })
-                    }
-                })
-                .catch((err) => {
-                    ElNotification({
-                        title: "Error",
-                        message: err.code == "ECONNABORTED" ? "更新超时" : "更新失败",
-                        type: "error"
-                    })
-                })
-        }
-    }
-})
+    delete_repo() {
+      this.axios
+        .post(
+          `${baseUrl}/api/repo/${this.$route.params.username}/${this.$route.params.reponame}/settings/delete`,{},{
+            withCredentials: true,
+          }
+        )
+        .then((data) => {
+          let code = data.data.status.code;
+          if (code == 200) {
+            ElNotification({
+              title: "Success",
+              message: "Delete repo successfully",
+              type: "success",
+            });
+            window.location.href= `${baseUrl}/${this.$route.params.username}`;
+          } else {
+            ElNotification({
+              title: "Error",
+              message: "Fail to delete repo",
+              type: "error",
+            });
+          }
+        })
+        .catch((err) => {
+          ElNotification({
+            title: "Error",
+            message: err.code == "ECONNABORTED" ? "超时" : "Fail to delete repo",
+            type: "error",
+          });
+        });
+    },
+    submit() {
+      this.axios
+        .post(
+          `${baseUrl}/api/repo/${this.$route.params.username}/${this.$route.params.reponame}/updateMetaData`,
+          this.nameForm,
+          {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((data) => {
+          let code = data.data.status.code;
+          if (code == 200 || code == -1002) {
+            console.log("login success");
+          } else if (code == -1003) {
+            ElNotification({
+              title: "Error",
+              message: "更新失败",
+              type: "error",
+            });
+          }
+        })
+        .catch((err) => {
+          ElNotification({
+            title: "Error",
+            message: err.code == "ECONNABORTED" ? "更新超时" : "更新失败",
+            type: "error",
+          });
+        });
+    },
+  },
+  beforeRouteEnter(_from, _to, next) {
+    next((vm) => (vm as any).reload());
+  },
+});
 </script>
 
 <style scoped>
