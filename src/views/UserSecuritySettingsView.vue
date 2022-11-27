606d12ff7abec53efe4f6656256c7d0379d3f600
Trust_04zh <trust04zh@gmail.com>
2022-11-27 19:50:25 +0800

    support updating email, pw

--- src/views/UserSecuritySettingsView.vue
+++ src/views/UserSecuritySettingsView.vue
@@ -1,16 +1,137 @@
 <template>
-    <div>
-      security setting (demo)
-      update email
-      update pw
-    </div>
-  </template>
-  
+  <el-row class="settings" style="width:100%;">
+    <el-col class="text-settings" :span="16">
+      Change email
+      <el-divider />
+      <el-form :model="formEmail" label-width="120px">
+        <el-form-item label="Email">
+          <el-input v-model="formEmail.email" :placeholder="old_email" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="updateEmail">Update</el-button>
+        </el-form-item>
+      </el-form>
+      Change password
+      <el-divider />
+      <el-form :model="formPassword" label-width="120px">
+        <el-form-item label="New password">
+          <el-input v-model="formPassword.password" show-password="true"/>  
+        </el-form-item>
+        <el-form-item label="Verification code">
+          <el-input v-model="formPassword.code" />  
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="updatePassword">Update</el-button>
+          <el-button type="primary" @click="sendCode">Send verification code</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+    <el-col :span="8"></el-col>
+  </el-row>
+</template>
+
 <script lang="ts">
 import { defineComponent } from "vue"
 
+import { baseUrl } from "@/stores/configs"
+import { userStore } from "@/stores/user"
+
 export default defineComponent({
+  data() {
+    return {
+      old_email: "",
+      formEmail: {
+        email: "",
+      },
+      formPassword: {
+        password: "",
+        code: "",
+      },
+    }
+  },
+  methods: {
+    mount() {
+      if (userStore().username == undefined) {
+        userStore().fillName().then(() => {
+          this.fillOldUserinfo()
+        })
+      } else {
+          this.fillOldUserinfo()
+      }
+    },
+    fillOldUserinfo() {
+      this.axios.get(`${baseUrl}/api/user/${userStore().username}`)
+        .then(res => res.data.data)
+        .then(data => {
+          console.log(data)
+          this.old_email = data.email
+        })
+    },
+    updateEmail() {
+      if (this.formEmail.email) {
+        const emailFormData = new FormData()
+        emailFormData.append('email', this.formEmail.email)
+        this.axios.post(`${baseUrl}/api/user/updateEmail`, emailFormData, {
+          withCredentials: true
+        }).then(data => {
+          console.log('update email: ')
+          console.log(data.data)
+          if (data.data.status.code == 200) {
+            this.old_email = this.formEmail.email
+          } else {
+            console.log('update email failed')
+          }
+        })
+      }
+    }, 
+    updatePassword() {
+      if (this.formPassword.password) {
+        const pwFormData = new FormData()
+        pwFormData.append('email', this.old_email)
+        pwFormData.append('code', this.formPassword.code)
+        pwFormData.append('password', this.formPassword.password)
+        this.axios.post(`${baseUrl}/api/user/resetPassword`, pwFormData, {
+          withCredentials: true
+        }).then(data => {
+          console.log('update password: ')
+          console.log(data.data)
+          if (data.data.status.code != 200) {
+            console.log('update password failed')
+          }
+        })
+      }
+    },
+    sendCode() {
+      const codeFormData = new FormData()
+      codeFormData.append('email', this.old_email)
+      this.axios.post(`${baseUrl}/api/user/sendResetPasswordEmail`, codeFormData, {
+        withCredentials: true
+      }).then(data => {
+        console.log('send code: ')
+        console.log(data.data)
+        if (data.data.status.code != 200) {
+          console.log('send code failed')
+        }
+      })
+    }
+  },
+  beforeMount() {
+      this.mount()
+  }
 })
 
 </script>
   
+<style scoped>
+
+.settings {
+  margin: 2rem;
+  display: flex;
+  flex-direction: column;
+}
+
+.text-settings {
+  margin-right: 2rem;
+}
+
+</style>
