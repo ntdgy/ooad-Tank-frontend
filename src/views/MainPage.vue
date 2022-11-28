19280e5312b2778c744873c696029cdbf1a82212
Frank <FrankssWu@outlook.com>
2022-11-28 13:41:12 +0800

    add mainPageBeforeLogin

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,12 +1,16 @@
 <template>
-    <el-link href="/login">login</el-link>
-    <el-link href="/yuki/yuki-public">test repo</el-link>
-    <el-link href="/yuki">test user</el-link>
+  <iframe
+    src="../../../../../MainPageBeforeLogin.html"
+    frameborder="0"
+    class="iframeBox"
+  ></iframe>
 </template>
 
 <style scoped>
-.el-link {
-    font-size: var(--el-font-size-large);
-    margin: 1rem;
+.iframeBox {
+  width: 100%;
+  height: 100%;
+  z-index: 99;
+  position: absolute;
 }
 </style>
