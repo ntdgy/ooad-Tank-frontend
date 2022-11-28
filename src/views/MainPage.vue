0dd644193671cf2ecfdf829ed6b209e21717604f
Frank <FrankssWu@outlook.com>
2022-11-28 17:43:16 +0800

    upd mainPage, add header

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,10 +1,11 @@
 <template>
-  <iframe
-    src="../../../../../MainPageBeforeLogin.html"
-    frameborder="0"
-    class="iframeBox"
-  ></iframe>
-
+  <div class="top-box" id="top-box">
+    <iframe
+      src="../../../../../MainPageBeforeLogin.html"
+      frameborder="0"
+      class="iframeBox"
+    ></iframe>
+  </div>
   <!-- <el-link href="/login">login</el-link>
     <el-link href="/yuki/testHtml">test repo</el-link>
     <el-link href="/yuki">test user</el-link>
@@ -12,10 +13,15 @@
 </template>
 
 <style scoped>
-.iframeBox {
+.top-box {
+  margin-top: 60px;
   width: 100%;
   height: 100%;
   z-index: 99;
   position: absolute;
+}
+.iframeBox {
+  width: 100%;
+  height: 100%;
 }
 </style>
